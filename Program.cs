efbbfbde04c1913c3bc109d9cbfd7783029d7755
MilesVdW <miles.vandewetering@parivedasolutions.com>
2017-11-10 17:55:46 -0800

    Project setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OracleEvolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

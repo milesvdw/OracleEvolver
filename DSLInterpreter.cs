48693a762b019bdbe7c9702cc2a24fbb4f1e1625
MilesVdW <miles.vandewetering@parivedasolutions.com>
2017-11-10 17:07:13 -0800

    More DSL setup

--- DSLInterpreter.cs
+++ DSLInterpreter.cs
@@ -1,5 +1,19 @@
 public enum StatementType {
     MathOp
+    BoolOP
+}
+
+public enum BoolOpType {
+    And,
+    Or,
+    Not
+}
+
+public enum MathOpType {
+    Add,
+    Multiply,
+    Subtract,
+    Divide //do we even need this one? I suspect not but...
 }
 
 public class DSLInterpreter<ResultType, Statement> {
@@ -12,14 +26,37 @@ public class LeagueStatement {
 
 
 public class MathOp : LeagueStatement {
-    public StatementType LeagueStatementType = StatementType.MathOp
+    public StatementType LeagueStatementType = StatementType.MathOp;
     public MathOpType type;
     public int left;
     public int right;
 
     public MathOp() {};
+}
 
+public class If : LeagueStatement {
+    public StatementType LeagueStatementType = StatementType.MathOp;
+    public BoolOpType type;
+    public boolean condition;
+    public 
+}
 
+public class Bool {
+    public BoolOpType type;
+}
+
+public class And : Bool {
+    public Bool left;
+    public Bool right;
+}
+
+public class Or : Bool {
+    public Bool left;
+    public Bool right;
+}
+
+public class Not : Bool {
+    public Bool inner;
 }
 
 public class LeagueInterpreter : DSLInterpreter<int, LeagueStatement> {
@@ -27,7 +64,7 @@ public class LeagueInterpreter : DSLInterpreter<int, LeagueStatement> {
         if(statement is basic math) {
             //return result
         }
-        if(statement is conditional) {
+        if(statement is Bool) {
             //evaluate conditional, then return appropriate path
         }
     }
